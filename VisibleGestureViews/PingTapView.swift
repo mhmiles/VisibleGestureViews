87318a1eeae55d74e157f9d5f5ecd717626710ad
Miles Hollingsworth <mhmiles@me.com>
2016-08-31 14:43:30 -0700

    cleanup

--- VisibleGestureViews/PingTapView.swift
+++ VisibleGestureViews/PingTapView.swift
@@ -97,7 +97,5 @@ extension PingTapView: VisibleGestureView {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(remainingDuration*Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
             self.ringLayer.removeAllAnimations()
         }
-        
-        print("Stop")
     }
 }
