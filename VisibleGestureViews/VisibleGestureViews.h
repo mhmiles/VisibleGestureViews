dcfadd82b6e9a4ac64677265a0c5166e32b43389
Miles Hollingsworth <mhmiles@me.com>
2016-08-31 11:31:46 -0700

    Initial Commit

---
+++ VisibleGestureViews/VisibleGestureViews.h
@@ -0,0 +1,19 @@
+//
+//  VisibleGestureViews.h
+//  VisibleGestureViews
+//
+//  Created by Miles Hollingsworth on 8/31/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VisibleGestureViews.
+FOUNDATION_EXPORT double VisibleGestureViewsVersionNumber;
+
+//! Project version string for VisibleGestureViews.
+FOUNDATION_EXPORT const unsigned char VisibleGestureViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisibleGestureViews/PublicHeader.h>
+
+
